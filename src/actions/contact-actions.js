fbefa46dc06ef3d2657934fd9fb2cface175639d
seval2016 <sevalsenturk2016@gmail.com>
2024-08-09 00:34:15 +0300

    form validation

--- src/actions/contact-actions.js
+++ src/actions/contact-actions.js
@@ -1,8 +1,30 @@
 "use server";
+import { response,transformYupErrors,YupValidationError } from "@/helpers/form-validation";
 
+
+//1.aşama : gelen datayı json'a çevirme
 export const createContactAction = async (prevData, formData) => {
+  try {
+    const fields = convertFormDataToJson(formData);
 
-	const fields=Object.fromEntries(formData.entries()); //gelen datayı json'a çevirdik
+    //2.aşama : Gelen datanın formatını kontrol etme. Bunun için Yup kullanılır.
+    ContactSchema.validateSync(fields, { abortEarly: false });
 
-	console.log(fields)
+    const res = await createMessage(fields);
+    const data = await res.json();
+
+    if (!res.ok) {
+      return response(false,"",{});
+    }
+
+   // revalidatePath("/dashboard/admin");
+    return response(true,"Your message was sent",{});
+
+  } catch (err) {
+    if (err instanceof YupValidationError) {
+      return transformYupErrors(err.inner);
+    }
+
+    throw err;
+  }
 };
--- src/helpers/form-validation.js
+++ src/helpers/form-validation.js
@@ -1,2 +1,29 @@
-//Bu kod, bir işlemin sonucunu kolayca takip etmeyi sağlar ve kullanıcıya işlemin durumu hakkında geri bildirim vermek için kullanılır.
-export const initialResponse = { ok: false, message: "", errors: {} }; 
+
+export {ValidationError as YupValidationError} from "yup";
+
+export const convertFormDataToJson = (formData) => {
+  Object.fromEntries(formData.entries()); //gelen datayı json'a çevirdik
+};
+
+//Bu kod, bir işlemin sonucunu kolayca takip etmeyi sağlar ve kullanıcıya işlemin durumu hakkında
+// geri bildirim vermek için kullanılır.
+
+export const response = (ok, message, errors) => ({
+  ok,
+  message,
+  errors,
+});
+export const initialResponse = response(false, "", {});
+
+export const transformYupErrors = (errors) => { //error olması halinde okunur bir hata döndürmesi için
+	const errObject = {};
+	errors.forEach((error) => (errObject[error.path] = error.message));
+
+	return response(false, "", errObject);
+};
+
+export const isStringArray = (str) => {
+	if (!str) return false;
+	const arr = JSON.parse(str);
+	return Array.isArray(arr) && arr.length > 0;
+};
